e65f897f5ca7350836956addb2c311e3570f13b7
Tvisha Shetty <tvishalucky@gmail.com>
2026-01-04 23:18:46 +0530

    remove TypeScript completely and switch to JSX for Vercel

--- localbuzzz/src/main.jsx
+++ localbuzzz/src/main.jsx
@@ -2,13 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./bms.css";
 
-
-
-ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
